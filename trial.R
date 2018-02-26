67dcaf0ca96355fc7d824794bd6b3c052365f68f
YenanGuan <yenan.guan@outlook.com>
2018-02-25 23:45:07 -0600

    set up GitHub on RStudio

--- trial.R
+++ trial.R
@@ -1,3 +1,11 @@
+Download Git
+Terminal:
+git config --global user.name "YenanGuan"
+git config --global user.email "yenan.guan@outlook.com"
+
+RStudio:
+    New Project -> Version Control -> Git -> Copy GitHub link
+
 RStudio -> GitHub
 Save, commit, tick the project, push branch
 
